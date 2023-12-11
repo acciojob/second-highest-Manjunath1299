b4df5d300951f483edcdb35e8fbeca9d502fa0d6
Manjunath <66263712+Manjunath1299@users.noreply.github.com>
2023-12-11 19:16:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function secondHighest(arr) {
+			//Write your code here
+			let max=-Infinity,second=-Infinity;
+			for(let i=0;i<arr.length;i++){
+			   let num=arr[i];
+				if(num>max){
+					second=max;
+					max=num;
+				}
+				if(num<max && num>second){
+					second=num;
+				}
+			}
+			return second;
+		}
